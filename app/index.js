4ddb856fb8c7b9ee3364ad7add3072f17baafbec
Johannes Schneider <schneider@philippka.de>
2017-10-22 10:12:55 -0500

    First try landing page

---
+++ app/index.js
@@ -0,0 +1,10 @@
+/**
+ * Application entry point
+ */
+
+// Load application styles
+import 'styles/index.scss';
+import '../node_modules/kickstart-node/js/kickstart'
+// ================================
+// START YOUR APP HERE
+// ================================
